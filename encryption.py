a6e8984b95daa15183d1cbf4b05cade09268c72b
Sugam Kafle <95976724+sugamkafle@users.noreply.github.com>
2026-01-10 09:25:28 +0930

    Implement encryption function in encryption.py
    Added an encryption function to read from a raw text file and prepare for encryption.

---
+++ encryption.py
@@ -0,0 +1,8 @@
+#Encryption function
+def encrypt(shift1, shift2):
+    
+    raw_file = open("raw_text.txt", 'r') #open raw file 
+    encrypted_file = open('encrypted_text.txt', 'w') #open file for encryption
+    
+    text = raw_file.read() #read content 
+    new_ch = "" #init string
