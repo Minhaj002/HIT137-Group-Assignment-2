f47d054e00ea1581507a3552cf36b88b20e4e978
Sugam Kafle <95976724+sugamkafle@users.noreply.github.com>
2026-01-11 16:19:40 +0930

    Add recursive edge drawing function
    Implemented recursive function to draw an edge.

--- question3/question3_main.py
+++ question3/question3_main.py
@@ -2,3 +2,17 @@
 #import required modules from library
 import turtle as t
 import math
+
+#Draw one edge using recursion
+def draw_edge(length, depth):
+    if depth == 0:
+        t.forward(length)
+    else:
+        segment = length / 3 # Divide the length into three segments
+        draw_edge(segment, depth - 1)
+        t.left(60)
+        draw_edge(segment, depth - 1)
+        t.right(120)
+        draw_edge(segment, depth - 1)
+        t.left(60)
+        draw_edge(segment, depth - 1)
